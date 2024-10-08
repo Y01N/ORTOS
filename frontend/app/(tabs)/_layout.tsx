b14fdf580d01cac2d02f28cf1d6aeed6b5a75fd9
koltonknight <knighk4@rpi.edu>
2024-10-08 17:38:43 -0400

    New Tab and Page Experimentation

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -32,6 +32,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="new"
+        options={{
+          title: 'New Tab',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'happy' : 'happy-outline'} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
